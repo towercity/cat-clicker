d6927595885af08620f4068dfb504aaa99b6669c
Matthew Nerger <mnerger@lsu.edu>
2016-04-07 17:49:15 -0500

    Add cat levels

--- scripts/NEWapp.js
+++ scripts/NEWapp.js
@@ -1,9 +1,25 @@
 var ViewModel = function() {
-    this.score = ko.observable(0);
     this.name = ko.observable('Ponchatoula');
+    this.score = ko.observable(0);
     this.pronoun = ko.observable('him');
     this.img = ko.observable('img/the-ponch-1500.jpg');
     this.imgAttribution = ko.observable('Sara Wasserman');
+
+    this.level = ko.computed(function() {
+        if (this.score() < 10) {
+            return "Newborn";
+        } else if (this.score() < 20) {
+            return "Kitten";
+        } else if (this.score() < 30) {
+            return "Awkward Teenager, Not Quite Cat But Not Kitten";
+        } else if (this.score() < 40) {
+            return "Cat";
+        } else if (this.score() < 50) {
+            return "Senior";
+        } else {
+            return "Elder Sage";
+        }
+    }, this);
 
     this.incrementCounter = function() {
         this.score(this.score() + 1);
